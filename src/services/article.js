7b19238dc050ca99c9549fa8ade7508be0196b16
Loós András Péter <loosandraspeter@gmail.com>
2023-05-20 21:41:51 +0200

    removed visible api key

--- src/services/article.js
+++ src/services/article.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidApiKey = "8e19d9d5edmsh75d6bbf6d63d6a2p153343jsn93bae7ac18d8";
+const rapidApiKey = VITE_RAPID_API_KEY;
 // i know im exposing my api key and i could have stored it in an env for example variable but im too lazy to do that, and im on a free plan so it doesnt matter anyway
 export const articleApi = createApi({
   reducerPath: "articleApi",
