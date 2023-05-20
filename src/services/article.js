a59147ff02142385d4b144f312f540447790a889
Loós András Péter <loosandraspeter@gmail.com>
2023-05-20 21:48:01 +0200

    now i really fixed the env variable

--- src/services/article.js
+++ src/services/article.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidApiKey = VITE_RAPID_API_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 // i know im exposing my api key and i could have stored it in an env for example variable but im too lazy to do that, and im on a free plan so it doesnt matter anyway
 export const articleApi = createApi({
   reducerPath: "articleApi",
